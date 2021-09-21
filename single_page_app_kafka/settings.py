6a70a8aa188c3ce046297786e3b44501672a8da5
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-09-21 17:51:26 +0200

    add middleware

--- single_page_app_kafka/settings.py
+++ single_page_app_kafka/settings.py
@@ -55,11 +55,14 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django_kafka.middleware.SecureAllMiddleWare',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
